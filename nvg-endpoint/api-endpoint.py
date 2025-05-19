d7c7256f3f70797b47f273ca1f4211557356621e
Mikhail Krasnov <mikhailkrasnov@Mikhails-MacBook-Air.local>
2025-05-19 08:51:21 +0200

    fix types

--- nvg-endpoint/api-endpoint.py
+++ nvg-endpoint/api-endpoint.py
@@ -14,11 +14,11 @@ import torchvision.models as models
 model = models.resnet18(num_classes=24)
 model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
-#gdown.download('https://drive.google.com/uc?id=1cBPywP7OgrXFvHuEpEG-DPM9rp8wXM8n')
-#checkpoint = torch.load('resnet_weights.pt', map_location=torch.device('cpu'))
+gdown.download('https://drive.google.com/uc?id=1cBPywP7OgrXFvHuEpEG-DPM9rp8wXM8n')
+checkpoint = torch.load('resnet_weights.pt', map_location=torch.device('cpu'))
 
-#model.load_state_dict(checkpoint['model_state_dict'])
-#model.eval()
+model.load_state_dict(checkpoint['model_state_dict'])
+model.eval()
 
 def adjToEdgidx(adj_mat):
     edge_index = torch.from_numpy(adj_mat).nonzero().t().contiguous()
@@ -31,8 +31,9 @@ app = FastAPI()
 
 
 @app.post("/NVG")
-async def echo(data: torch.Tensor):
+async def echo(data: List[List[float]]):
     # print(data[:5])
+    data = torch.tensor(data)
     out = model(data)
     pred = out.argmax(dim=1)
     # print(pred)
